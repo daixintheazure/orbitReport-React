619eebb2bac1f8f3a2aab2bd88cd8917fed33bed
Mario Dickerson <dai.dickerson@gmail.com>
2023-08-12 07:42:08 -0500

    updated doc

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,23 @@
 import './styling.css';
 
 const Table = ({ sat }) => {
-  const satInfo = sat.map(stuff, id => {
+  const satInfo = sat.map(function(stuff) {
     return (
-        <tr key={id}>
+        <tr>
           <td>{stuff.name}</td>
           <td>{stuff.type}</td>
           <td>{stuff.launchDate}</td>
           <td>{stuff.orbitType}</td>
         </tr>
-    );
-  });
+  )})
+    
+  
+
   return (
       <table>
        <thead>
         <tr>
           <th>Name</th>
-        </tr>
-        <tr>
           <th>Type of Satellite</th>
           <th>Lanunch Date</th>
           <th>Status</th>
