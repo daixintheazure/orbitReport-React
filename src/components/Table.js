6a06d429157db9b397e4b396c23f49158ca87bd5
Mario Dickerson <dai.dickerson@gmail.com>
2023-08-13 06:02:08 -0500

    corrected spellings

--- src/components/Table.js
+++ src/components/Table.js
@@ -30,7 +30,7 @@ const Table = ({ sat }) => {
         <tr>
           <th>Name</th>
           <th>Type of Satellite</th>
-          <th>Lanunch Date</th>
+          <th>Launch Date</th>
           <th>Status</th>
         </tr>
         </thead>
